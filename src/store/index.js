d0a721315d399d705d9b162c270aa5d2a43dab02
jlozan64 <janeth2.jl@gmail.com>
2020-04-23 16:35:40 -0500

    added views

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,46 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from "vue"
+import Vuex from "vuex"
+import createPersistedState from "vuex-persistedstate"
 
-Vue.use(Vuex);
+import { db } from "../plugins/firebase"
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    user: '',
+  },
+  getters: {
+    getUser: (state) => state.user,
+  },
+  mutations: {
+    settingUser: (state, user) => (state.user = user),
+  },
+  actions: {
+    setUser: async ({ commit }, user) => {
+      if (user) {
+        const userObject = {
+          displayName: user.displayName,
+          email: user.email,
+          roles: {
+            user: true,
+          },
+          lastAccess: new Date(),
+        }
+        const userRef = db.collection("users").doc(user.uid)
+        await userRef.set(userObject, { merge: true })
+        const userDB = await userRef.get()
+        commit("settingUser", { uid: user.uid, ...userDB.data() })
+        await db.collection("cart").doc(user.uid).set({
+          items: [],
+          total: 0,
+        })
+      } else {
+        commit("settingUser", '')
+      }
+    },
+  },
+  plugins: [createPersistedState()],
+})
+
+export default store
